015b11798f2e4faa6aa8b2de8495f1d51cf65d05
yamunacheerpureddy <yamunacheerapureddy01@gmail.com>
2025-03-03 17:38:32 +0530

    Delete restapi directory

--- restapi/withrestc1/testapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from testapp.models import Employee
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['id','eno','ename','esal','eaddr']
-admin.site.register(Employee,EmployeeAdmin)
--- restapi/withrestc1/testapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-class EmployeeSerializer(serializers.Serializer):
-    eno = serializers.IntegerField()
-    ename = serializers.CharField(max_length=64)
-    esal = serializers.FloatField()
-    eaddr = serializers.CharField(max_length=64)
-    
