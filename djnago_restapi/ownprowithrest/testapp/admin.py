4dcc44c3056ecd71602883d2c3a02370c03ef846
yamunacheerpureddy <yamunacheerapureddy01@gmail.com>
2025-03-03 17:36:47 +0530

    Delete djnago_restapi directory

--- djnago_restapi/ownprowithrest/testapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Employee 
-# Register your models here.
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['id','eno','ename','esal','eaddrs']
-admin.site.register(Employee,EmployeeAdmin)
--- djnago_restapi/ownprowithrest/testapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework.viewsets import ModelViewSet
-from testapp.models import Employee
-from testapp.serializers import EmployeeSerializer
-class Employee(ModelViewSet):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
